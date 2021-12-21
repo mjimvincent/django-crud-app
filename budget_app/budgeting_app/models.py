13c540e0f989e0f52fc43630107b36942df7a096
iampaaatrick <iampaaatrick@gmail.com>
2021-12-21 22:30:30 +0400

    initial

--- budget_app/budgeting_app/models.py
+++ budget_app/budgeting_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+# class AccountInfo (models.Model):
+#     username = models.CharField(max_length=20)
+#     password = models.CharField(max_length=20)
+
+# class BudgetInfo (models.Model):
+#     user_budget = models
