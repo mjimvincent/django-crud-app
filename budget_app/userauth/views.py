ab7547fe74ef9b01c237c636e121bda2efefb0e4
Jim <mjimvincent@gmail.com>
2021-12-22 23:11:27 +0800

    minor edit (removed double import)

--- budget_app/userauth/views.py
+++ budget_app/userauth/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from django.shortcuts import redirect
 # from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
