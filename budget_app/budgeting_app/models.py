001c2f290cbf9369df1c4510493204d1901ebf5e
Jim <mjimvincent@gmail.com>
2021-12-25 20:19:01 +0800

    py

--- budget_app/budgeting_app/models.py
+++ budget_app/budgeting_app/models.py
@@ -1,10 +1,40 @@
 from django.db import models
+from django.contrib.auth import models as authmodel
 
 # Create your models here.
 
-# class AccountInfo (models.Model):
-#     username = models.CharField(max_length=20)
-#     password = models.CharField(max_length=20)
+class Project(models.Model):
+    created_by = models.ForeignKey(authmodel.User, on_delete=models.CASCADE,
+                                   related_name="%(app_label)s_%(class)s_related", related_query_name="%(app_label)s_%(class)ss")
+    project_users = models.ManyToManyField(authmodel.User,  related_name="%(app_label)s_%(class)s_related_users", related_query_name="%(app_label)s_%(class)ss_users")
+    name = models.CharField(max_length=50)
+    desc = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now=True)
 
-# class BudgetInfo (models.Model):
-#     user_budget = models
+class Budget(models.Model):
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    budget = models.DecimalField(decimal_places=3, max_digits=20)
+    created_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(authmodel.User, on_delete=models.CASCADE)
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    desc = models.TextField(null=True)
+    type = models.CharField(max_length=50)
+
+class Planned(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    amount = models.DecimalField(decimal_places=3, max_digits=20)
+    direction = models.CharField(max_length=50)
+
+class Cashflow(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(authmodel.User, on_delete=models.CASCADE)
+    amount = models.DecimalField(decimal_places=3, max_digits=20)
+    created_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=50)
+    desc = models.TextField(null=True)
+    direction = models.CharField(max_length=50)
+
